ddb268df1fddfb7ed14e25ff2404a8027d9cf51a
Kiriyaga W <kiriyaga7615@github.com>
2026-02-19 23:31:16 +0100

    upstream: 1.3.5

--- src/main/java/namidevelopment/kiriyaga/templateplugin/client/impl/command/UnbindAllCommand.java
+++ src/main/java/namidevelopment/kiriyaga/templateplugin/client/impl/command/UnbindAllCommand.java
@@ -2,25 +2,35 @@ package namidevelopment.kiriyaga.templateplugin.client.impl.command;
 
 import namidevelopment.kiriyaga.api.annotation.RegisterCommand;
 import namidevelopment.kiriyaga.api.model.command.Command;
-import namidevelopment.kiriyaga.api.model.command.CommandArgument;
-import net.minecraft.network.chat.Component;
-
+import namidevelopment.kiriyaga.api.model.command.CommandRoute;
 import namidevelopment.kiriyaga.api.model.feature.Feature;
 import namidevelopment.kiriyaga.api.model.setting.KeyBindSetting;
+import net.minecraft.network.chat.MutableComponent;
+
+import java.util.List;
 
 import static namidevelopment.kiriyaga.api.NamiApi.*;
 
+@RegisterCommand
 public class UnbindAllCommand extends Command {
 
     public UnbindAllCommand() {
-        super("unbindall", new CommandArgument[0]);
+        super("unbindall");
     }
 
     @Override
-    public void execute(Object[] parsedArgs) {
+    public CommandRoute[] getRoutes() {
+        return new CommandRoute[] {
+                new CommandRoute(null)
+        };
+    }
 
+    @Override
+    public void execute(String route, Object[] args) {
         int count = 0;
-        for (Feature feature : FEATURE_SERVICE.getStorage().getAll()) {
+
+        List<Feature> features = FEATURE_SERVICE.getStorage().getAll();
+        for (Feature feature : features) {
             for (var setting : feature.getSettings()) {
                 if (setting instanceof KeyBindSetting bindSetting) {
                     if (bindSetting.get() != 0) {
@@ -31,8 +41,8 @@ public class UnbindAllCommand extends Command {
             }
         }
 
-        Component msg = CAT_FORMAT.format("{gray}Removed {global}" + count + " {gray}binds from all your features.");
-        CHAT_SERVICE.sendPersistent(UnbindAllCommand.class.getName(), msg);
+        MutableComponent msg = CAT_FORMAT.format("{gray}Removed {global}" + count + " {gray}binds from all your features.");
+        CHAT_SERVICE.sendPersistent(this.getName(), msg);
 
         CONFIG_SERVICE.saveFeatures();
     }
